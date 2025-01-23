83ac162e96b5067c60889bb7737a28c490137b51
David Besen <dbesen@gmail.com>
2025-01-23 12:50:38 -0700

    Add initial test

---
+++ chatgpt_test.py
@@ -0,0 +1,19 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),  # This is the default and can be omitted
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Say this is a test",
+        }
+    ],
+    model="gpt-4o",
+)
